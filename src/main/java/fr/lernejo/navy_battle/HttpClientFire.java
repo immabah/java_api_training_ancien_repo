4f77150bd3ab7573ec8f987a39872f7f04ce4dc1
Immaculee Bahoundje <immaculee.bahoundje@epita.fr>
2022-03-30 01:42:20 +0200

    Do Fire Api

---
+++ src/main/java/fr/lernejo/navy_battle/HttpClientFire.java
@@ -0,0 +1,25 @@
+package fr.lernejo.navy_battle;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientFire {
+    private final HttpClient Clt = HttpClient.newHttpClient();
+
+    public void Clientfire(String url, String cell) {
+        String requestPt = url + "/api/game/fire?cell=" + cell;
+        HttpRequest getRequest = HttpRequest.newBuilder().uri(URI.create(requestPt))
+            .setHeader("Accept", "application/json")
+            .setHeader("Content-Type", "application/json").GET().build();
+
+        try {
+            HttpResponse<String> response = Clt.send(getRequest, HttpResponse.BodyHandlers.ofString());
+
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/fr/lernejo/navy_battle/HttpClient_navy.java
+++ src/main/java/fr/lernejo/navy_battle/HttpClient_navy.java
@@ -9,18 +9,16 @@ import java.net.http.HttpResponse;
 public class HttpClient_navy {
     private final HttpClient client;
     public final int port;
-    public final String url;
 
-    public HttpClient_navy(int port, String url) {
+    public HttpClient_navy(int port) {
         this.client = HttpClient.newHttpClient();
         this.port = port;
-        this.url = url;
 
     }
 
-    public void SendStart() throws IOException, InterruptedException {
+    public void SendStart(String url) throws IOException, InterruptedException {
         HttpRequest requestPost = HttpRequest.newBuilder()
-            .uri(URI.create(this.url + "/api/game/start"))
+            .uri(URI.create(url + "/api/game/start"))
             .setHeader("Accept", "application/json")
             .setHeader("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + this.port + "\", \"message\":\"hello\"}"))            .build();
