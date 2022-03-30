aa13bc09c8d09c485ebcf78910693aa2872b5c77
Immaculee Bahoundje <immaculee.bahoundje@epita.fr>
2022-03-30 17:52:41 +0200

    Add modif to firehandle

--- src/main/java/fr/lernejo/navy_battle/HttpClient_navy.java
+++ src/main/java/fr/lernejo/navy_battle/HttpClient_navy.java
@@ -9,14 +9,15 @@ import java.net.http.HttpResponse;
 public class HttpClient_navy {
     private final HttpClient client;
     public final int port;
-
-    public HttpClient_navy(int port) {
+    public final String url;
+    public HttpClient_navy(int port, String url) {
         this.client = HttpClient.newHttpClient();
         this.port = port;
+        this.url = url;
 
     }
 
-    public void SendStart(String url) throws IOException, InterruptedException {
+    public void SendStart() throws IOException, InterruptedException {
         HttpRequest requestPost = HttpRequest.newBuilder()
             .uri(URI.create(url + "/api/game/start"))
             .setHeader("Accept", "application/json")
