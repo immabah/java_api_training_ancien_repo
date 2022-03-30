4fbefad37670ab26e04290d48a71e56283a2f663
Immaculee Bahoundje <immaculee.bahoundje@epita.fr>
2022-03-30 20:21:07 +0200

    Esperons que ça marche enfin

--- src/main/java/fr/lernejo/navy_battle/HttpPostHandler.java
+++ src/main/java/fr/lernejo/navy_battle/HttpPostHandler.java
@@ -5,7 +5,6 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 
 public class HttpPostHandler implements HttpHandler {
     public final int port;
@@ -17,17 +16,16 @@ public class HttpPostHandler implements HttpHandler {
 
     public void handle(HttpExchange httpExchange) throws IOException {
         if (httpExchange.getRequestMethod().equals("POST")) {
-            Response(httpExchange);
+            Response(httpExchange, "Ok");
         } else {
             httpExchange.sendResponseHeaders(404, -1);
         }
+        httpExchange.close();
     }
-    public void Response(HttpExchange httpExchange) throws IOException{
+    public void Response(HttpExchange httpExchange, String body) throws IOException{
         String msg = "{\"id\":\"1\", \"url\":\"http://localhost:" + this.port + "\", \"message\":\"hello\"}";
-        String recev = new String(httpExchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
-        System.out.println(recev);
         httpExchange.getResponseHeaders().set("Content-type", "application/json");
-        httpExchange.sendResponseHeaders(202, msg.length());
+        httpExchange.sendResponseHeaders(202, body.length());
         try (OutputStream os = httpExchange.getResponseBody()){
             os.write(msg.getBytes());
         }
